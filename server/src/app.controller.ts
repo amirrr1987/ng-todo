d6d78629cbc0a6dfcfc9dba3c04215f51bc9ba3e
Amir Maghami <Maghami.A1987@gmail.com>
2024-12-30 10:40:16 +0330

    Update app.controller.ts

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, UsePipes } from '@nestjs/common';
+import { Controller, Get, Logger, UsePipes } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GetUser } from './auth/common/get-user.decorator';
+import { UserEntity } from './users/entities/user.entity';
 
 @Controller()
 export class AppController {
+  private logger = new Logger('App controller');
   constructor(private readonly appService: AppService) {}
 
   @Get()
   @UsePipes()
-  getHello(): string {
+  getHello(@GetUser() user: UserEntity): string {
+    this.logger.verbose(
+      `User call App User: ${JSON.stringify(user?.username ?? '-')}`,
+    );
     return this.appService.getHello();
   }
 }
