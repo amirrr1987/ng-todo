b6d0001f01bd57e500b9b56473a4225584fb6a4a
Amir Maghami <Maghami.A1987@gmail.com>
2024-12-30 10:32:57 +0330

    feat: add .env Docker file and compose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,11 +12,11 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5434,
-      username: 'amir',
-      password: '0251',
-      database: 'task-management',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       // autoLoadEntities: true,
       synchronize: true,
       logging: true,
